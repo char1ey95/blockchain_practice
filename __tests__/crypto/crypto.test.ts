d95738852a8faacd691f95ce9d8870bb4f47d8ac
char1ey <ckstn410@gmail.com>
2023-04-25 10:44:20 +0900

    feat: crypto.binary test 케이스 작성

--- __tests__/crypto/crypto.test.ts
+++ __tests__/crypto/crypto.test.ts
@@ -9,11 +9,21 @@ describe('CryptoModule', () => {
 
     describe('SHA256', () => {
         it('SHA256에 인자내용을 평문으로해서 암호화가 되는가', () => {
-            const data = "hello world"
+            const data = '이전 블록을 이용해서 데이터르 만들어야한다.'
             // SHA 256
             // hello world => 16진수 내용으로 뽑히게
             const result = cryptoModule.SHA256(data)
             expect(result.length).toBe(64)
+        })
+    })
+
+    describe('HashtoBinary', () => {
+        it('이진데이터로 잘 변경되는가?', () => {
+            const data = 'hash'
+            const hash = cryptoModule.SHA256(data)
+            
+            const binary = cryptoModule.hexToBinary(hash)
+            expect(binary.length).toBe(256)
         })
     })
 })
--- src/core/crypto/crypto.module.ts
+++ src/core/crypto/crypto.module.ts
@@ -6,6 +6,24 @@ class CryptoModule {
         const hash: Hash = cryptojs.SHA256(data).toString()
         return hash as Hash
     }
+
+    // 0    0    => 16진수 : 변화이 쉽기 때문에 사용한다
+    // 0000 0000 =>  2진수
+
+    // 16진수
+    // 0 1 2 3
+    // 4 5 6 7
+    // 8 9 A B
+    // C D E F
+
+    // 네자리 이진수는 16가지로 나타낼 수 있다(2^4 = 16)
+    // 0000 0001 0010 0011
+    // 0100 0101 0110 0111
+    // 1000 1001 1010 1011
+    // 1100 1101 1110 1111
+    hexToBinary(hash: Hash): string {
+        return ''
+    }
 }
 
 export default CryptoModule
