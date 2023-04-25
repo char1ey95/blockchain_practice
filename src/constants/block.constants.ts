8a714f7f99c2c119721273cdc754b9061b3b0b17
char1ey <ckstn410@gmail.com>
2023-04-25 09:34:23 +0900

    feat: block interface 설정

---
+++ src/constants/block.constants.ts
@@ -0,0 +1,11 @@
+const GENESIS = {
+    version: '1.0.0',
+    height: 1, // Block의 높이
+    timestamp: 1231006506,
+    previousHash: '0'.repeat(64), // 이전 블록의 해쉬 // 32바이트 64글자
+    merkleRoot: '0'.repeat(64), // transaction // hash값이 될 것(string) // 32바이트 64글자
+    nonce: 0, // PoW를 할 때 필요한 속성 중 하나
+    difficulty: 0, // PoW를 할 때 필요한 속성 중 하나
+    hash: '0'.repeat(64), // 다양한 속성값을 이용해서 구성할 것
+    data: "2009년 1월 3일 더 타임스, 은행들의 두번째 구제금융을 앞두고 있는 U.K 재무장관", // transaction들이 모일 곳, 배열 형태가 될것
+}
---
+++ src/types/block.d.ts
@@ -0,0 +1,7 @@
+// type : 타입에 의미를 부여
+// 아래는 number인데 Height라는 의미를 가진다.
+
+export type Height = number
+export type Timestamp = number
+export type Difficulty = number
+export type Hash = string
