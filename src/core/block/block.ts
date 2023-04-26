ad0f638f2f7d28684dbbe0f5fccb774f21c2c59c
char1ey <ckstn410@gmail.com>
2023-04-26 11:20:29 +0900

    delete: 모든 주석 내용 삭제

--- src/core/block/block.ts
+++ src/core/block/block.ts
@@ -11,12 +11,11 @@ export default class Block {
         if (validHash !== block.hash) throw new Error(`블록해시값이 올바르지 않습니다 hash : ${validHash}, ${block.hash}`)
     }
 
-    // Block 인스턴스가 생성되면, BlockInfo 모양의 객체를 반환해준다.
     createBlockInfo(previousBlock: IBlock): BlockInfo {
         this.isValidBlock(previousBlock)
 
         const blockInfo = new BlockInfo()
-        
+
         blockInfo.version = VERSION
         blockInfo.height = previousBlock.height + 1
         blockInfo.timestamp = new Date().getTime()
